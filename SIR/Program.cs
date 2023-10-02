e8c97fda5e691ee3fe5d0ac1bbff7960e4cf95d1
Ellen Koning <ellenkoning707@gmail.com>
2023-10-02 20:03:05 +0200

    added folder structure

--- SIR/Program.cs
+++ SIR/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 string path = "../../../../data/";
 Filer filer = new Filer(path);
-Demographic demographic = new Demographic();
-Disease disease = new Disease();
+Demographic demographic = new Demographic(7,0,0,0.1m,50,10);
+Disease disease = new Disease(0.95m,0, 0, 0);
 Model model = new Model(filer, demographic, disease);
-model.run(500, false);
+model.run(100, true);
